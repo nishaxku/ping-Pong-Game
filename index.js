38b7d0879f88075c05f9f27e4d4361bf0761eca9
nishaxku <nishakumbhkar65@gmail.com>
2026-03-20 10:26:28 +0530

    paddle moving up and down

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded",() => {
     let ball = document.getElementById('ball');
     let table =document.getElementById('ping-pong-table');
+    let paddle = document.getElementById('paddle')
+
      let ballx = 50
      let bally = 50
      let dx = 2;
@@ -21,6 +23,21 @@ document.addEventListener("DOMContentLoaded",() => {
 
 
      },1);
+
+
+     let paddleY = 0;
+     let pdy = 5;
+
+     document.addEventListener('keydown',(event) => {
+        if(event.keyCode == 38 && paddleY > 0 ){
+            paddleY += (-1)*pdy;;
+            console.log("up",paddleY)
+        }else if(event.keyCode == 40 && paddleY < table.offsetHeight - paddle.offsetHeight){
+            paddleY += pdy;
+
+        }
+        paddle.style.top = `${paddleY}px`;
+     })
 
      
 });
